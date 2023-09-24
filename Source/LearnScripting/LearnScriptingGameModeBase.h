b5c72e91a43fd3799a50c38cf93d3d7cf0ffaa9d
sreng <stickercenter2@gmail.com>
2023-09-24 17:36:46 +0700

    Getting Start UE 5

---
+++ Source/LearnScripting/LearnScriptingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearnScriptingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNSCRIPTING_API ALearnScriptingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
